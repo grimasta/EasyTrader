55c1df6227ecf05918181499f483f22996dc222b
Grimasta <Grimasta@users.noreply.github.com>
2026-09-03 00:59:53 -0400

    updated __init__.py for venues/bitget_v2/api_client to enable proper execution of tests on github CI

--- venues/bitget_v2/api_client/__init__.py
+++ venues/bitget_v2/api_client/__init__.py
@@ -1,3 +1,8 @@
 from .v2_bitget_api_coms import API_KEY
 from .v2_bitget_api_coms import API_SECRET
 from .v2_bitget_api_coms import API_PASSPHRASE
+from .background_order_tpsl_coordinator import (
+    handle_sl_if_any,
+    ExitEvent,
+    _EXIT_EVENTS,
+)
